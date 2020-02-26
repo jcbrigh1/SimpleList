54c83a81f62642821f284dbbceb398aabfe3526b
Jason Bright <piano7jcb@gmail.com>
2020-02-26 13:57:44 -0700

    First Version

---
+++ cse360assign2/src/cse360assign2/SimpleList.java
@@ -0,0 +1,127 @@
+package cse360assign2;
+
+public class SimpleList {
+	/**
+	 * This is a class that represents a list that stores 10 integers
+	 * and can manipulate the entries
+	 */
+	private int[] list;
+	private int count;
+	
+	/**
+	 * Default constructor, creates array of 10 integers
+	 * and sets the count to 0
+	 */
+	public SimpleList()
+	{
+		list = new int[10];
+		count = 0;
+	}
+	
+	/**
+	 * this method will an integer to the list
+	 * @param k, number to be added
+	 */
+	public void add(int k) 
+	{
+		/**
+		 * if there are no elements in the array, then we can add 
+		 * the new element directly into the first part of the array
+		 */
+		if(count == 0) 
+		{                  
+			count++;
+			list[0] = k;
+		}
+		/**
+		 * if there is more than one element in the array then all elements
+		 * should be shifted over one unit and the number is thrown into the 
+		 * first element of the array
+		 */
+		else {
+			if(count == 10) 
+			{                
+				count--;	 
+			}
+			for(int i = count; i > 0; i--)
+			{
+				list[i]= list[i-1];
+			}
+			list[0] = k;
+			count++;
+		}
+	}
+
+	/**
+	 * the method remove is designed to remove the first instance 
+	 * of an element of the array
+	 * @param k, element to be removed
+	 */
+	public void remove(int k) 
+	{
+		int i = 0;
+		boolean removed = false;
+		while(removed == false && i < count)
+		{
+			if(list[i] == k) //bring elements back starting at element to be removed
+			{
+				for(int j = i; j <= count; j++)
+				{
+					list[j] = list[j+1]; //element is the element in front
+				}
+				removed = true;
+				count--;
+			}
+			i++;
+		}
+	}
+	/**
+	 * count method returns the amount of elements in the array
+	 * similar to a "getter" method for the variable count
+	 * @return count, the number of elements in the list
+	 */
+	public int count() 
+	{
+		return count;
+	}
+	
+	/**
+	 * toString overrides Java's built in toString method, in this case
+	 * returning each element with a space in between
+	 * @return String of Array
+	 */
+	public String toString() 
+	{
+		String str = "";
+		for(int i = 0; i < count - 1; i++)
+		{
+			str = str + list[i] + " "; //adding each element to the string
+		}
+		if(count > 0)
+			str = str + list[count-1];//adding the last elements, without a space
+		return str;
+	}
+	
+	/**
+	 * search method will return the first instance of the
+	 * number asked to be searched, if there is no search result found
+	 * it will return -1
+	 * @param k, number to be searched
+	 * @return element position of k
+	 */
+	public int search(int k)
+	{
+		boolean found = false;
+		int location = -1;		//setting default to -1, should it not be found
+		for(int i = 0; i < count && found == false; i++) 
+		{
+			if(list[i] == k) //looking for the element
+			{
+				location = i;
+				found = true;
+			}
+		}
+		return location; //returning where the element is
+	}
+
+}
---
+++ cse360assign2/src/cse360assign2/SimpleListTester.java
@@ -0,0 +1,114 @@
+package cse360assign2;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class SimpleListTester {
+	private SimpleList myList = new SimpleList();
+	
+	@Test
+	void testConstructor() {
+		SimpleList aList = new SimpleList(); //testing to make sure it is not null
+		assertNotNull(aList);
+	}
+
+	@Test
+	void testaddOneElement() //adding one element, testing for correct array
+	{
+		myList.add(5);
+		assertEquals("5", myList.toString());
+	}
+	
+	@Test
+	void testAddTwoElements() //adding two elements, testing for correct array
+	{
+		myList.add(5);
+		myList.add(4);
+		assertEquals("4 5", myList.toString());
+	}
+	
+	@Test
+	void testAddMoreThanTenElements() //adding more than 10 elements, testing for
+	{								  //correct "fall off" of last elements
+		myList.add(7);	//1
+		myList.add(4);	//2
+		myList.add(2);	//3
+		myList.add(9);	//4
+		myList.add(13);	//5
+		myList.add(32);	//6
+		myList.add(2);	//7
+		myList.add(61);	//8
+		myList.add(42);	//9
+		myList.add(91); //10
+		assertEquals("91 42 61 2 32 13 9 2 4 7", myList.toString());
+		myList.add(31);
+		assertEquals("31 91 42 61 2 32 13 9 2 4", myList.toString());
+		myList.add(53);
+		assertEquals("53 31 91 42 61 2 32 13 9 2", myList.toString());	
+	}
+	
+	@Test
+	void testRemove() //testing remove method
+	{
+		myList.add(7);	//1
+		myList.remove(7);
+		assertEquals("", myList.toString()); //testing with one element
+		myList.add(7);	//1
+		myList.add(4);	//2
+		myList.add(2);	//3
+		myList.add(9);	//4
+		myList.add(13);	//5
+		myList.add(32);	//6
+		myList.add(2);	//7
+		myList.remove(9);
+		assertEquals("2 32 13 2 4 7", myList.toString()); //testing with multiple elements
+	}
+	
+	@Test
+	void testCount() //testing the count method, with different amount of elements
+	{
+		assertEquals(0, myList.count());
+		myList.add(4);	//1
+		assertEquals(1, myList.count());
+		myList.add(2);	//2
+		assertEquals(2, myList.count());
+		myList.add(2);	//3
+		myList.add(9);	//4
+		myList.add(13);	//5
+		myList.add(26);	//6
+		myList.add(2);	//7
+		myList.add(9);	//8
+		myList.add(13);	//9
+		assertEquals(9, myList.count());
+		myList.add(43);	//10
+		assertEquals(10, myList.count());
+		myList.add(98); //11
+		assertEquals(10, myList.count());
+	}
+	
+	@Test
+	void testSearch() //testing search method with different numbers of elements
+	{
+		myList.add(3);
+		assertEquals("0", myList.search(3)+"");
+		myList.add(6);
+		assertEquals("0", myList.search(6)+"");
+		assertEquals("1", myList.search(3)+"");
+		myList.add(9);
+		myList.add(12);
+		myList.add(15);
+		myList.add(18);
+		myList.add(21);
+		assertEquals("2", myList.search(15)+"");
+		myList.add(24);
+		myList.add(27);
+		myList.add(30);
+		myList.add(33);
+		myList.add(36);
+		myList.add(39);
+		assertEquals("9", myList.search(12)+"");
+		assertEquals("-1", myList.search(23)+"");
+	}
+}
